813c697c27c4f55b3f1a662463b10e43fc177f61
Kimberlly <136401578+Kimberllynmr@users.noreply.github.com>
2024-10-23 20:35:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Armazenar os dados 
+let dados = [];
+
+// Gerar ID aleatório com 5 dígitos
+function gerarld() {
+    return Math.floor(10000 + Math.random() * 90000);
+}
+
+// Adicionar dados à tabela
+document.getElementByld('form').addEventListener('submit', function(event) {
+   event.preventDefault()
+
+   const descricao = 
+
+
+
+)
+
+}
